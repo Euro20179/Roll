07159ccfff464a151f297eb5267e539982b22e8e
Euro <no>
2021-09-26 13:55:23 -0700

    gitignore

--- src/js/start.js
+++
@@ -1,7 +0,0 @@
-let start = true;
-function prepareValues() {
-    const rollCountInput = document.getElementById("roll-count-input");
-    start = false;
-    //@ts-ignore
-    window.location = `./game.html?rolls=${rollCountInput.value}`;
-}
